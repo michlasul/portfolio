e228966e31ef719c183c9c4bb610d380897eaca3
Muhammad Ichlasul Amal <muhammadichlasulamal00@gmail.com>
2023-01-01 00:50:17 +0700

    Memperbaiki sedikit masalah di btn header

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const btnBerubah = document.querySelector('#btn-berubah');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        btnBerubah.classList.add('btn-berubah');
     } else {
         header.classList.remove('navbar-fixed');
+        btnBerubah.classList.remove('btn-berubah');
     }
 };
 
