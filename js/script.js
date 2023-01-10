db577198df4cb8d637ccb4ad214256e838c4385d
Muhammad Ichlasul Amal <muhammadichlasulamal00@gmail.com>
2023-01-10 12:04:30 +0700

    Buat harga

--- js/script.js
+++ js/script.js
@@ -4,15 +4,17 @@ window.onscroll = function () {
     const fixedNav = header.offsetTop;
     const btnBerubah = document.querySelector('#btn-berubah');
 
-    if (window.pageYOffset > fixedNav) {
+
+    if (window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
         btnBerubah.classList.add('btn-berubah');
-    } else {
+
+    } else{
         header.classList.remove('navbar-fixed');
         btnBerubah.classList.remove('btn-berubah');
     }
 
-    
+
 };
 
 // Hamburger
@@ -26,7 +28,7 @@ hamburger.addEventListener('click', function() {
 
 // klik diluar hamburger
 window.addEventListener('click', function(e){
-    if (e.target != hamburger && e.target != navMenu) {
+    if (e.target !== hamburger && e.target !== navMenu) {
         hamburger.classList.remove('hamburger-active');
         navMenu.classList.add('hidden');
     }
